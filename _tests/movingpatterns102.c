ef23e2afc42ffd74be39e40d03a51fd6344108b1
Christopher Konopka <cskonopka@gmail.com>
2021-03-11 22:24:42 -0500

    added new experiments

---
+++ _tests/movingpatterns102.c
@@ -0,0 +1,47 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.02, pct, st.y) -
+          smoothstep( pct, pct+0.02, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    st -= 1.80*cos(0.32);  // becomes -0.5 to 0.5
+    st *= 12.0;
+    float pct2 = 0.0;
+    pct2 = distance(st,vec2(0.5));
+
+    float y = (smoothstep((sin((sin(st.y*st.x))*(PI*cos(u_time*0.13212))))*(sin((st.x)*0.20)),2.29005,0.32));
+    // float y = (smoothstep((cos((sin(st.y))+u_time*0.3212))/(cos((st.x)*0.2301)),0.9005,(sin((st.y)*3.301))/sin((st.x)*.301)));
+    float y2 = (smoothstep(1.1,0.5,(st.y/st.x)+1.2));
+    float y3 = sin(tan((st.y+(PI*sin(u_time*0.242)+st.x)*0.94)+(0.590)))+sin(st.x*0.12) + cos(sin((st.y)*(-2.13)))+sin(PI*st.y*1.12);
+    // vec3 colorA = vec3(y)*1.0;
+    vec3 colorA = vec3(y+y2+y3)*1.0;
+    colorA = (1.0)*colorA*vec3(.281990,0.4120*+cos(st.x),(0.139230*(sin(y3+st.y*(u_time*0.27012)))));
+
+    float y4 = (smoothstep((sin((sin(st.y*st.x))*(PI*cos(u_time*0.13212))))*(sin((st.x)*0.20)),20.29005,0.32));
+    // float y = (smoothstep((cos((sin(st.y))+u_time*0.3212))/(cos((st.x)*0.2301)),0.9005,(sin((st.y)*3.301))/sin((st.x)*.301)));
+    float y5 = (smoothstep(1.1,0.5,(st.y/st.x)+1.2));
+    float y6 = cos(((st.y+(sin(u_time*0.242)+st.x)*2.94)+(0.590)))*sin(st.x*2.12) + cos(sin((st.y)*(-2.13)))+sin(PI*st.y*1.12);
+
+
+    vec3 colorB = vec3(y4+y5+y6)*1.0;
+    colorB = (1.0)*colorB*vec3(3.281990,0.04120-cos(st.x),(1.139230*(cos(y3+st.y*(u_time*0.27012)))));
+
+
+    vec2 bl = step(vec2(0.5),(st));
+    float pct = bl.x * bl.y;
+    vec3 colorMix = vec3(0.0);
+    colorMix = mix(colorB, colorA*2.2, 0.2);
+
+    gl_FragColor = vec4(colorMix,1.0);
+}
---
+++ _tests/movingpatterns119.c
@@ -0,0 +1,39 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+#define PI 3.14159265359
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+
+float plot(vec2 st, float pct){
+  return  smoothstep( pct-0.02, pct, st.y) -
+          smoothstep( pct, pct+0.02, st.y);
+}
+
+void main() {
+    vec2 st = gl_FragCoord.xy/u_resolution;
+    st -= 1.80*cos(0.32);  // becomes -0.5 to 0.5
+    st *= 12.0;
+    float pct2 = 0.0;
+    pct2 = distance(st,vec2(0.5));
+
+    float y = (smoothstep((sin(-pct2*((sin(st.y+st.x))*0.001)*(cos(0.913212))))*(tan((st.x)*00.20)),(0.229005),(0.32)))/(smoothstep((sin(pct2*((cos(st.y+st.x))*0.2001)*(cos(0.913212))))*(tan((st.x)*01.20)),(0.229005),(0.32)));
+    // float y = (smoothstep((cos((sin(st.y))+u_time*0.3212))/(cos((st.x)*0.2301)),0.9005,(sin((st.y)*3.301))/sin((st.x)*.301)));
+
+    float y2 = (smoothstep((sin(-pct2*(sin(st.y/st.x)*0.9591321)-(sin(.1913212))))*(tan((-st.x+st.y)*1.20)),0.229005,0.2832/cos(-st.y*st.x*0.020))) / (smoothstep((cos(-pct2*(sin(st.y*(st.x*0.02))*0.1321)-(tan(u_time*0.13212))))+(cos((st.y)*0.20)),0.229005,0.32));
+
+    vec3 colorB = vec3(y*y2)*1.0;
+    colorB = (1.0)*colorB*vec3(.9281990,0.94120*tan((st.x*st.y)*0.02),(0.4139230*(sin(y2-st.y*(u_time*0.7012))+st.x))+tan(st.y/st.x));
+    colorB = (1.0)*colorB*vec3(.9281990,0.94120*cos((st.x+sin(st.y/st.x))*0.02),(0.4139230*(sin(y+st.y*(u_time*0.127012))+st.y)));
+
+
+    vec2 bl = step(vec2(0.5),(st));
+    float pct = bl.x * bl.y;
+    vec3 colorMix = vec3(0.0);
+    colorMix = mix(colorB, colorB*0.3, 0.2);
+
+    gl_FragColor = vec4(colorMix,1.0);
+}
